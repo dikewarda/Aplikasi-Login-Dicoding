8954f1c673b5db83c439859378b45fff92aa29f3
dikewarda <dikeayuw@gmail.com>
2023-09-13 21:05:10 +0700

    edit login script

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -17,12 +17,12 @@ loginFormElement.addEventListener('submit', function(event) {
     // TODO 1 : Mendapatkan input email dan password pengguna dari form
     const email = inputEmailElement.value;
     const password = inputPasswordElement.value;
-  
+
     /*  TODO 2 : Buat Logika perbandingan dengan kondisi:
         Jika variabel email identik dengan expectedEmail dan password identik dengan expectedPassword,
         panggil fungsi goToHome()
         Jika tidak, maka panggil fungsi showPopUp()
-     */
+    */
     if (email == expectedEmail && password == expectedPassword) {
         goToHome();
     } else {
